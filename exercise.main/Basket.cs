765e227211d0c6ca5aac79e93a8643d7d508edec
Stian Forren <stian.forren@experisacademy.no>
2025-08-11 11:35:05 +0200

    Stian Forren

---
+++ exercise.main/Basket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise.main
+{
+    public class Basket
+    {
+
+        private int _basketSize;
+        public List<IProduct> ProductList { get; set; }
+
+        public Basket(int basketSize)
+        {
+            _basketSize = basketSize;
+        }
+
+
+        public void Add(IProduct product)
+        {
+
+        }
+    }
+}
---
+++ exercise.tests/CoreTest.cs
@@ -0,0 +1,25 @@
+﻿using exercise.main;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise.tests
+{
+    [TestFixture]
+    public class CoreTest
+    {
+        [Test]
+        public void TestAdd()
+        {
+            Basket basket = new Basket(3);
+            IProduct bagel = new Bagel();
+            basket.Add(bagel);
+
+            Assert.That(basket.ProductList.Count == 1);
+
+
+        }
+    }
+}
